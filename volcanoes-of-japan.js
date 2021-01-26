59cbf40ead4e8047c884b700e857deadb5895bad
Felix Palmer <felixpalmer@gmail.com>
2021-01-26 10:12:27 +0100

    typo

--- volcanoes-of-japan.js
+++ volcanoes-of-japan.js
@@ -7,7 +7,7 @@ const title = document.getElementById( 'title' );
 const subtitle = document.getElementById( 'subtitle' );
 
 // Define API Keys (replace these with your own!)
-const NASADEM_APIKEY = null';
+const NASADEM_APIKEY = null;
 if ( !NASADEM_APIKEY ) {
   const error = Error( 'Modify index.html to include API keys' );
   container.innerHTML = error; 
